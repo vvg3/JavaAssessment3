c680975658a525608cd5987153addbedbdfedeb8
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-03-19 10:47:56 -0400

    first save

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -1,16 +1,60 @@
 package parsing_json;
 
-public class ElementCollection {
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class ElementCollection extends ArrayList<Element> {
 
     public Element findByAtomicNumber(int atomic_number) {
-        return null;
+
+        Element match = new Element();
+
+        for (Element element : this) {
+            if (element.getNumber() == atomic_number) {
+                match = element;
+            }
+        }
+        return match;
     }
 
     public Element findByName(String name) {
-        return null;
-    }
+        Element match = new Element();
+
+        for (Element element : this) {
+            if (element.getName().equalsIgnoreCase(name)) {
+                match = element;
+            }
+        }
+        return match;    }
 
     public ElementCollection where(String fieldName, Object value) {
-        return null;
+
+        ElementCollection elementsThatMatchValueOnField = new ElementCollection();
+
+        if (classContainsAttribute(fieldName)) {
+
+            for (int i = 0; i < this.size(); i++) {
+                try {
+                    if (this.get(i).getClass().getDeclaredField(fieldName).equals(value)) {
+                        elementsThatMatchValueOnField.add(this.get(i));
+                    }
+                } catch (NoSuchFieldException nsfe) {
+                    nsfe.printStackTrace();
+                }
+            }
+        }
+
+        return elementsThatMatchValueOnField;
+    }
+
+    private boolean classContainsAttribute(String fieldName) {
+        Class elementClass = Element.class;
+        Field[] fieldNames = elementClass.getFields();
+        for (Field name : fieldNames) {
+            if (name.getName().equals(fieldName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/parsing_json/ElementCollectionInitializer.java
+++ src/main/java/parsing_json/ElementCollectionInitializer.java
@@ -1,7 +1,15 @@
 package parsing_json;
 
+
 public class ElementCollectionInitializer {
+
     public static ElementCollection generate() {
+
+
+
+
+
         return null;
     }
+
 }
